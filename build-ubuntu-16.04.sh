0b4af19d6439198a0ac8e3e02aa2d9d8c194a3b5
Bin Shao <binshao@live.com>
2018-04-03 17:09:01 +0800

    updates build-ubuntu-16.04.sh

--- build-ubuntu-16.04.sh
+++ build-ubuntu-16.04.sh
@@ -3,6 +3,7 @@ set -eux -o pipefail
 export DV_COPT_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-O3"
 # TensorFlow 1.7.0
 export DV_TENSORFLOW_GIT_SHA="024aecf414941e11eb643e29ceed3e1c47a115ad"
+export DV_PACKAGE_CURL_PATH="https://storage.googleapis.com/deepvariant/packages"
 
 # install prerequisite
 sudo -H apt-get -qq -y update
