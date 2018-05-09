52b902a675947a3c8ace86e25503422444d7cd01
Bin Shao <binshao@live.com>
2018-05-09 16:47:37 +0800

    update quick-start.sh

--- quick-start.sh
+++ quick-start.sh
@@ -18,8 +18,8 @@ gsutil cp -R "${DATA_BUCKET}" "${WORKSPACE}"
 
 OUTPUT_DIR="${WORKSPACE}/output"
 mkdir -p "${OUTPUT_DIR}"
-REF="${WORKSPACE}quickstart-testdata/ucsc.hg19.chr20.unittest.fasta"
-BAM="${WORKSPACE}quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam"
+REF="${WORKSPACE}/quickstart-testdata/ucsc.hg19.chr20.unittest.fasta"
+BAM="${WORKSPACE}/quickstart-testdata/NA12878_S1.chr20.10_10p1mb.bam"
 MODEL="${WORKSPACE}/${MODEL_NAME}/model.ckpt"
 
 echo "Running make_examples"
@@ -30,7 +30,7 @@ N_SHARDS=3
 mkdir -p "${LOGDIR}"
 time seq 0 $((N_SHARDS-1)) | \
   parallel --eta --halt 2 --joblog "${LOGDIR}/log" --res "${LOGDIR}" \
-  python "${BIN_DIR}"/make_examples.zip \
+  python "${BIN_DIR}/make_examples.zip" \
     --mode calling \
     --ref "${REF}" \
     --reads "${BAM}" \
@@ -38,3 +38,22 @@ time seq 0 $((N_SHARDS-1)) | \
     --regions '"chr20:10,000,000-10,010,000"' \
     --task {}
 
+echo "Running call_variants"
+
+CALL_VARIANTS_OUTPUT="${OUTPUT_DIR}/call_variants_output.tfrecord.gz"
+
+python "${BIN_DIR}/call_variants.zip" \
+ --outfile "${CALL_VARIANTS_OUTPUT}" \
+ --examples "${OUTPUT_DIR}/examples.tfrecord@${N_SHARDS}.gz" \
+ --checkpoint "${MODEL}"
+
+ echo "Running postprocess_variants"
+
+ FINAL_OUTPUT_VCF="${OUTPUT_DIR}/output.vcf.gz"
+
+python "{BIN_DIR}/postprocess_variants.zip" \
+  --ref "${REF}" \
+  --infile "${CALL_VARIANTS_OUTPUT}" \
+  --outfile "${FINAL_OUTPUT_VCF}"
+
+echo "Done"
